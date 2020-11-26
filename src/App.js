eb9b3e0ce453d5e06226448cc852fd5e24dd05d9
Лидия <lidia@MacBook-Pro-Lidia.local>
2020-11-26 13:22:46 +0200

    added bootstrep components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+    CardDeck,
+    CardBody,
+    CardHeader,
+    Card,
+    Col,
+    Button,
+    CardFooter,
+    CardText
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            dataList: [{
+                key: 1,
+                listName: 'to do',
+            }, {
+                key: 2,
+                listName: "doing",
+            }, {
+                key: 3,
+                listName: "done",
+            }],
+            dataCards: [
+                {
+                    key: 1,
+                    cardName: "final project",
+                    keyList: 1
+                }, {
+                    key: 2,
+                    cardName: "trello",
+                    keyList: 2
+                }, {
+                    key: 3,
+                    cardName: "node js",
+                    keyList: 3
+                }, {
+                    key: 4,
+                    cardName: "react app",
+                    keyList: 3
+                }, {
+                    key: 5,
+                    cardName: "final project",
+                    keyList: 1
+                },
+
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <CardDeck>
+                    {this.state.dataList.map((list, index) => {
+                        const cards = this.state.dataCards.filter(card => card.keyList === list.key);
+                        return (
+                            <div key={index}>
+                                <Col sm="2.5">
+                                    <Card>
+                                        <CardHeader>
+                                            {list.listName}
+                                            <Button close/>
+                                        </CardHeader>
+                                        {
+                                            cards.map((card, index) => {
+                                                return (
+                                                    <CardBody key={index}>
+                                                        <div>
+                                                            <CardText>
+                                                                {card.cardName}
+                                                                <Button close/>
+                                                            </CardText>
+                                                        </div>
+                                                    </CardBody>
+                                                )
+                                            })
+                                        }
+                                        <CardFooter>
+                                            <div className="row">
+                                                <input/>
+                                                <Button>Save</Button>
+                                            </div>
+                                        </CardFooter>
+                                    </Card>
+                                </Col>
+                            </div>
+                        )
+                    })}
+                </CardDeck>
+            </div>
+        )
+    }
 }
 
 export default App;
